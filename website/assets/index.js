1f9d1f5df9d312337c63c6e885a21a4771c9c7f7
reffcp <yo@cristiancovarrubias.online>
2020-06-23 07:34:41 -0500

    primer commit

---
+++ website/assets/index.js
@@ -0,0 +1,23 @@
+const video = document.querySelector("video")
+const button = document.querySelector("button")
+
+
+function mediaPlayer(config){
+  this.media= config.el
+}
+
+mediaPlayer.prototype.play = function(){
+  this.media.play()
+}
+mediaPlayer.prototype.pause = function(){
+  this.media.pause()
+}
+
+const player = new mediaPlayer({el: video});
+button.onclick = ()  => {
+  if (player.media.paused) {
+    player.play()
+  } else {
+    player.pause()
+  }
+};
